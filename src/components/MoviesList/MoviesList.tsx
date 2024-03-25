7b8cbc98c89b317f1131e0f67944a07a5e67b970
Konrad Wasilewski <146724743+konradwasilewski444@users.noreply.github.com>
2024-03-25 16:01:44 +0100

    Revert "version1"

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,18 +1,4 @@
 import React from 'react';
 import './MoviesList.scss';
-import { Movie } from '../../types/Movie';
-import { MovieCard } from '../MovieCard';
 
-interface Props {
-  movies: Movie[];
-}
-
-export const MoviesList: React.FC<Props> = ({ movies }) => {
-  return (
-    <div className="movies">
-      {movies.map(movie => {
-        return <MovieCard movie={movie} key={movie.imdbId} />;
-      })}
-    </div>
-  );
-};
+export const MoviesList: React.FC = () => <>Put the list here</>;
--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,7 +1,5 @@
 export interface Movie {
   title: string;
-  description: string;
-  imgUrl: string;
-  imdbUrl: string;
-  imdbId: string;
+
+  // add all the other field
 }
